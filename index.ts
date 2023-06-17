53c403583d4725b125c465714a4e99a7cfd6b872
Keme Kenneth <keme.kenneth@gmail.com>
2023-06-17 04:59:46 +0100

    api response

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT
 const app:Express = express()
 
 app.get('/status', (req:Request, res:Response) => {
-    res.json({status: 'all looking good!!!'})
+    res.json({status: 'all looking good!'})
 })
 
 app.listen(PORT, () => console.log('App started on port', PORT))
